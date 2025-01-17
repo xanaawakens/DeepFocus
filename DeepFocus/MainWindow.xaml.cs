423dc417005a92f6e3b8671b6e720aa6434eadf2
Tuan <avixiii@proton.me>
2025-01-17 07:52:13 +0700

    Create modern UI
    - Added Colors.xaml and Styles.xaml for consistent theming
    - Updated App.xaml with resource dictionaries
    - Created modern main window UI with timer display and controls
    - Added custom titlebar and window management
    - Implemented NavigationView for future pages

--- DeepFocus/MainWindow.xaml.cs
+++ DeepFocus/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -10,8 +11,11 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Windowing;
+using WinRT.Interop;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using DeepFocus.ViewModels;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,14 +27,34 @@ namespace DeepFocus
     /// </summary>
     public sealed partial class MainWindow : Window
     {
+        public MainViewModel ViewModel { get; }
+
         public MainWindow()
         {
             this.InitializeComponent();
-        }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
-        {
-            myButton.Content = "Clicked";
+            // Get AppWindow
+            var hWnd = WindowNative.GetWindowHandle(this);
+            var windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
+            var appWindow = AppWindow.GetFromWindowId(windowId);
+
+            // Customize titlebar
+            ExtendsContentIntoTitleBar = true;
+            SetTitleBar(AppTitleBar);
+
+            // Set initial size
+            appWindow.Resize(new Windows.Graphics.SizeInt32 { Width = 1000, Height = 600 });
+
+            // Set minimum size
+            var presenter = appWindow.Presenter as OverlappedPresenter;
+            presenter.IsResizable = true;
+            presenter.IsMaximizable = true;
+            presenter.IsMinimizable = true;
+            presenter.SetMinSize(new Windows.Graphics.SizeInt32 { Width = 500, Height = 400 });
+
+            // Initialize ViewModel
+            ViewModel = App.Current.Services.GetService(typeof(MainViewModel)) as MainViewModel 
+                ?? throw new InvalidOperationException("MainViewModel not found in DI container");
         }
     }
 }
